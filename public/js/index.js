06406a44a0d93788e70a42a775fe98db919416ea
Luke Rees <escrew7@hotmail.com>
2018-02-22 06:50:38 +0800

    scroll to bottom if user is close to bottom

--- public/js/index.js
+++ public/js/index.js
@@ -1,5 +1,21 @@
 var socket = io()
 
+function scrollToBottom() {
+  //selectors
+  var messages = jQuery('#messages')
+  var newMessage = messages.children('li:last-child')
+  //heights
+  var clientHeight = messages.prop('clientHeight')
+  var scrollTop = messages.prop('scrollTop')
+  var scrollHeight = messages.prop('scrollHeight')
+  var newMessageHeight = newMessage.innerHeight()
+  var lastMessageHeight = newMessage.prev().innerHeight()
+
+  if(clientHeight + scrollTop + newMessageHeight + lastMessageHeight >= scrollHeight) {
+    messages.scrollTop(scrollHeight)
+  }
+}
+
 socket.on('connect', () => {
   console.log('connected to server')
 })
@@ -17,6 +33,7 @@ socket.on('newMessage', (message) => {
     createdAt: formattedTime
   })
   jQuery('#messages').append(html)
+  scrollToBottom()
 })
 
 var messageTextBox = jQuery('[name=message]')
@@ -30,6 +47,7 @@ socket.on('newLocationMessage', function(message) {
     createdAt: formattedTime
   })
   jQuery('#messages').append(html)
+  scrollToBottom()
 })
 
 jQuery("#message-form").on('submit', function (e) {
