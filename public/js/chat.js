2d443f444ce75ba47d78be299229f249008e07de
Luke Rees <escrew7@hotmail.com>
2018-03-05 13:32:01 +0800

    send messages to only people in same room

--- public/js/chat.js
+++ public/js/chat.js
@@ -73,7 +73,6 @@ jQuery("#message-form").on('submit', function (e) {
   e.preventDefault()
 
   socket.emit('createMessage', {
-    from: 'user',
     text: messageTextBox.val()
   }, function () {
     messageTextBox.val('')
